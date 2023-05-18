a04503ba87b7cbc5a0178bc558f73e990ea53108
arifremal <arifremal013@gmail.com>
2023-05-18 22:20:28 +0600

    homepage hero section added

--- src/AuthComponents/Login.jsx
+++ src/AuthComponents/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
     const loginSubmit = event=>{
         event.preventDefault()
@@ -15,15 +17,15 @@ const Login = () => {
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="text" placeholder="email" name="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="text" placeholder="password" name="password" className="input input-bordered" />
           <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            <p>New to Toy Mart <Link className="text-green-500" to={"/signup"}>SignUp</Link> </p>
           </label>
         </div>
         <div className="form-control mt-6">
