3a9303e24e69768ead50a168009243670a5e37e2
arifremal <arifremal013@gmail.com>
2023-05-19 11:28:50 +0600

    all routes and component created done such as home,header,foorer,all toys,my toys,add a toy ,blog

--- src/Components/ToysCard.jsx
+++ src/Components/ToysCard.jsx
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom";
 
-const ToysCard = ({toy}) => {
-    const {_id,title,img,price}= toy
+const ToysCard = ({ toy }) => {
+  const {
+    _id,
+    title,
+    img,
+    price,
+    sub_category,
+    name,
+    available_quantity,
+    seller_name,
+  } = toy;
   return (
     <div className="card w-96 bg-base-100 shadow-xl image-full">
       <figure>
-        <img
-          src={img}
-          alt="Shoes"
-        />
+        <img src={img} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{title}</h2>
+        <h2 className="card-title">{name}</h2>
+        <p>Sub Category:{sub_category}</p>
         <p>Price:$ {price}</p>
+        <p>Available Quantity:{available_quantity}</p>
         <div className="card-actions justify-end">
-         <Link to={`/view/${_id}`} className=""> <button className="btn bg-green-500 ">View Details</button></Link>
+          <Link to={`/view/${_id}`} className="">
+            {" "}
+            <button className="btn bg-green-500 ">View Details</button>
+          </Link>
         </div>
       </div>
     </div>
--- src/ToyComponents/AddaToy.jsx
+++ src/ToyComponents/AddaToy.jsx
@@ -6,6 +6,9 @@ const AddaToy = () => {
                  <h1 className="text-center text-3xl font-bold p-2 uppercase ">
        Add a <span className="text-green-500"> Toy </span>
       </h1>
+      <div>
+        
+      </div>
         </div>
     );
 };
