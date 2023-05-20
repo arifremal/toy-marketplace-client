6be3072552daab6fd44cdfd451a59163f900f067
arifremal <arifremal013@gmail.com>
2023-05-20 14:30:51 +0600

    seller reviee and get in touch section added as extra section

--- src/Components/GallerySingle.jsx
+++ src/Components/GallerySingle.jsx
@@ -9,21 +9,28 @@ const GallerySingle = ({ toy }) => {
     name,
   } = toy;
   return (
-    <div className="card w-96 bg-base-100 shadow-xl image-full">
-      <figure>
-        <img src={picture_url} alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>Sub Category:{sub_category}</p>
+    // <div className="card w-96 bg-base-100 shadow-xl image-full">
+    //   <figure>
+    //     <img src={picture_url} alt="Shoes" />
+    //   </figure>
+    //   <div className="card-body">
+    //     <h2 className="card-title">{name}</h2>
+    //     <p>Sub Category:{sub_category}</p>
 
-        <div className="card-actions justify-end">
-          <Link to={`/view/${_id}`} className="">
-            {" "}
-            <button className="btn bg-green-500 ">View Details</button>
-          </Link>
-        </div>
-      </div>
+    //     <div className="card-actions justify-end">
+    //       <Link to={`/view/${_id}`} className="">
+    //         {" "}
+    //         <button className="btn bg-green-500 ">View Details</button>
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+ <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-white rounded-box">
+  <div className="carousel-item">
+    <img src={picture_url} className="rounded-box" />
+</div>
+</div>
     </div>
   );
 };
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,8 +8,10 @@ const Home = () => {
         <div className="hero min-h-screen bg-white p-4">
           <div className="hero-content  flex-col lg:flex-row">
             <img
-              className="rounded-lg p-3"
-              src={"https://i.ibb.co/3vWV2CK/rsz-71uv2p59vcl.jpg"}
+              className="rounded-lg p-3 w-1/2 "
+              src={
+                "https://media.cnn.com/api/v1/images/stellar/prod/210310140916-barbie-dolls-play-stock.jpg?q=x_3,y_156,h_1684,w_2993,c_crop/h_720,w_1280"
+              }
             />
             <div>
               <h1 className="text-5xl font-bold">
@@ -19,6 +21,40 @@ const Home = () => {
                 WHERE LEGENDS ARE MADE BACKGROUND ARTIST CREDIT – MICHAEL BELL
                 AKA @3_SHOTS3
               </p>
+              <div className="avatar-group -space-x-6">
+                <div className="avatar">
+                  <div className="w-12">
+                    <img
+                      src={
+                        "https://cdn.vox-cdn.com/thumbor/SiU8hkQH47M9a6W6jo65HxCVKoY=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22189115/jbareham_201221_0988_american_girl.jpg"
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="avatar">
+                  <div className="w-12">
+                    <img
+                      src={
+                        "https://www.gannett-cdn.com/presto/2019/11/05/USAT/aead9a73-6324-4135-b76c-e15d0921b14f-Full_Size_American_Girl_1.jpg"
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="avatar">
+                  <div className="w-12">
+                    <img
+                      src={
+                        "https://images-na.ssl-images-amazon.com/images/I/71x8m8uqRyL._AC_UL600_SR600,600_.jpg"
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="avatar placeholder">
+                  <div className="w-12 bg-green-500 text-neutral-content">
+                    <span>+10</span>
+                  </div>
+                </div>
+              </div>
               <button className="btn  bg-green-500 border-none hover:bg-black">
                 Get Started
               </button>
@@ -27,33 +63,6 @@ const Home = () => {
         </div>
       </div>
 
-      {/* gallery */}
-      {/* <div>
-    
-<div className="carousel carousel-center rounded-box">
-  <div className="carousel-item">
-    <img src={"https://i.ibb.co/JkWSf9P/1.png"}  alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src={"https://i.ibb.co/JkWSf9P/1.png"}  alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src={"https://i.ibb.co/JkWSf9P/1.png"}  alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src={"https://i.ibb.co/JkWSf9P/1.png"}  alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src={"https://i.ibb.co/JkWSf9P/1.png"}  alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src={"https://i.ibb.co/JkWSf9P/1.png"}  alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src={"https://i.ibb.co/JkWSf9P/1.png"} alt="Pizza" />
-  </div>
-</div>
-</div> */}
       <h1 className="text-center text-3xl font-bold p-2 uppercase ">
         Toy <span className="text-green-500">Gallery</span>
       </h1>
@@ -65,6 +74,76 @@ const Home = () => {
           Shop by <span className="text-green-500">category</span>
         </h1>
         <ShoByCategory></ShoByCategory>
+      </div>
+
+      <h1 className="text-center text-3xl font-bold p-2 uppercase ">
+        Seller <span className="text-green-500">Review</span>
+      </h1>
+      <div className="text-center py-4 ">
+        <div className="avatar indicator text-center mr-20">
+          <span className="indicator-item badge bg-black">Nice Product</span>
+          <div className="w-20 h-20 rounded-full">
+            <img src={'https://static.vecteezy.com/system/resources/thumbnails/007/486/093/small/portrait-of-happy-redhaired-woman-employee-in-optical-glasses-has-satisfied-expression-works-with-modern-gadgets-waits-for-meeting-with-colleague-prepares-accounting-report-sits-in-own-cabinet-free-photo.jpg'} />
+          </div>
+        </div>
+        <div className="avatar indicator text-center mr-20">
+          <span className="indicator-item badge bg-black">Good Quality</span>
+          <div className="w-20 h-20 rounded-full">
+            <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTviW8N10VfHUqOrhXMDQcMTLt1GuZ9XXm8OgsSHH0GDoccu8Pra7UXEmawxI6XrF7p_JA&usqp=CAU'} />
+          </div>
+        </div>
+        <div className="avatar indicator text-center me-5">
+          <span className="indicator-item badge bg-black">My kids love's so much</span>
+          <div className="w-20 h-20 rounded-full">
+            <img src={'https://blog.hubspot.com/hs-fs/hubfs/employee-retention-rate.jpg?width=595&height=400&name=employee-retention-rate.jpg'} />
+          </div>
+        </div>
+    
+      </div>
+
+      <h1 className="text-center text-3xl font-bold p-2 uppercase ">
+        Get in <span className="text-green-500">touch</span>
+      </h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row p-6">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Contact US!</h1>
+            <p className="py-6">
+              {" "}
+              If you have any question.just fill up this form.Don't forget to
+              stay connect with us{" "}
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Message</span>
+                </label>
+                <textarea
+                  name=""
+                  id=""
+                  cols="10"
+                  rows="5"
+                  className="border-2"
+                ></textarea>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn bg-green-500 border-none">Send</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
